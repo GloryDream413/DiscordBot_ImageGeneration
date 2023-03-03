fc8ebe39af039a1bb151924bfbc47337247ce660
GloryDream413 <putnikdeveloperbg@gmail.com>
2023-03-03 04:58:31 +0100

    ImageGenerationDiscordBot Initializing..

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# Discord bot
+
+```
+pip install -r requirements.txt
+```
+### dev mode
+```
+python3 mybot.py
+```
+### product mode
+```
+pm2 start mybot.py --name mybot --interpreter python3
+```
+```
+pm2 stop mybot.py
+```
---
+++ messages.py
@@ -0,0 +1,5 @@
+message = {
+    "/start": """👋 Hello there!\n\nThis bot returns AI-generated images from any prompt you give.\n<b>Example:</b> Send a message like <pre>/imagine a gorgeous supermodel with blonde hair</pre>\n\nEnjoy! 💖""",
+    "/help": "Send a message like <pre>/imagine a gorgeous supermodel with blonde hair</pre> to generate a image.",
+    "/wait":"The image is being generated. Please wait a few seconds for it..."
+}
